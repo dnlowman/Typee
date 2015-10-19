fceacb6f69e5be2166670d8db91a4a1f9e97d677
Daniel Lowman <dnlowman@gmail.com>
2015-10-19 21:26:01 +0100

    Wrapped states into an object

--- typee.js
+++ typee.js
@@ -1,4 +1,4 @@
-function typee(className, sentences, typeSpeed, sentenceChangeInterval, sentenceRemovalInterval) {
+            function typee(className, sentences, typeSpeed, sentenceChangeInterval, sentenceRemovalInterval) {
                 if(typeof(className) === 'undefined') {
                     console.warn('Typee requires a className!');
                     return;
@@ -8,46 +8,50 @@ function typee(className, sentences, typeSpeed, sentenceChangeInterval, sentence
                     console.warn('Typee requires at least one sentence!');
                     return;
                 }
+
                 sentenceChangeInterval = (typeof(sentenceChangeInterval) === 'undefined') ? 3000 : sentenceChangeInterval;
                 sentenceRemovalInterval = (typeof(sentenceRemovalInterval) === 'undefined') ? 3000 : sentenceRemovalInterval;
                 typeSpeed = (typeof(typeSpeed) === 'undefined') ? 200 : typeSpeed;
+
                 var currentSentence = 0;
                 var currentSentenceCharacter = 0;
                 var displayText = '';
 
-                var REMOVING_PAUSED = 0;
-                var CHANGING_SENTENCE_PAUSED = 1;
-                var REMOVING = 2;
-                var TYPING = 3;
-                var state = TYPING;
+                var states = {
+                    REMOVING_PAUSED: 0,
+                    CHANGING_SENTENCE_PAUSED: 1,
+                    REMOVING: 2,
+                    TYPING: 3
+                };
+                var state = states.TYPING;
 
                 setInterval(function() {
-                    if(state === REMOVING_PAUSED || state === CHANGING_SENTENCE_PAUSED) return;
-                    if(currentSentenceCharacter === sentences[currentSentence].length && state === TYPING) {
-                        state = REMOVING_PAUSED;
+                    if(state === states.REMOVING_PAUSED || state === states.CHANGING_SENTENCE_PAUSED) return;
+                    if(currentSentenceCharacter === sentences[currentSentence].length && state === states.TYPING) {
+                        state = states.REMOVING_PAUSED;
                         setTimeout(function() {
                             isTyping = false;
-                            state = REMOVING;
+                            state = states.REMOVING;
                         }, sentenceRemovalInterval);
                     }
 
-                    if(currentSentenceCharacter === 0 && state === REMOVING) {
-                        state = CHANGING_SENTENCE_PAUSED;
+                    if(currentSentenceCharacter === 0 && state === states.REMOVING) {
+                        state = states.CHANGING_SENTENCE_PAUSED;
                         displayText = '';
                         currentSentence = (currentSentence === sentences.length - 1) ? 0 : ++currentSentence;
                         setTimeout(function() {
-                            state = TYPING;
+                            state = states.TYPING;
                         }, sentenceChangeInterval);
                     }
 
                     switch(state) {
-                        case TYPING: {
+                        case states.TYPING: {
                             displayText += sentences[currentSentence].slice(currentSentenceCharacter, currentSentenceCharacter + 1);
                             currentSentenceCharacter++;
                             break;
                         }
 
-                        case REMOVING: {
+                        case states.REMOVING: {
                             displayText = sentences[currentSentence].slice(0, currentSentenceCharacter);
                             currentSentenceCharacter--;
                             break;
